8646ba33da8478de62861dd0f6ed3046e9880503
Mehul Shekhar Mukherjee <mehulshekharmukherjee@Mehuls-MacBook-Pro.local>
2022-01-03 19:43:01 -0500

    added pretier

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-
-    const [ activeIndex, setActiveIndex ] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(null);
 
     const onTitleClick = (index, active) => {
-
-        if(active) {
+        if (active) {
             index = null;
         }
 
@@ -14,23 +12,25 @@ const Accordion = ({ items }) => {
     };
 
     const renderedItems = items.map((item, index) => {
-
         const active = index === activeIndex ? 'active' : '';
 
         return (
             <React.Fragment key={item.title}>
-                <div className={`title ${active}`} onClick={() => onTitleClick(index, active)}>
-                    <i className='dropdown icon'></i>
+                <div
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(index, active)}
+                >
+                    <i className="dropdown icon"></i>
                     {item.title}
                 </div>
                 <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
-        )}
-    );
+        );
+    });
 
-    return <div className='ui styled accordion'>{renderedItems}</div>;
-}
+    return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
 export default Accordion;
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-
-    const [ term, setTerm ] = useState('');
-    const [ results, setResults ] = useState([]);
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-
         const search = async () => {
-            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term
+            const { data } = await axios.get(
+                'https://en.wikipedia.org/w/api.php',
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: term,
+                    },
                 }
-            });
-
+            );
             setResults(data.query.search);
         };
 
         const timeOutId = setTimeout(() => {
-            if(term) {
+            if (term) {
                 search();
-            }
-            else {
+            } else {
                 setResults([]);
             }
         }, 1000);
@@ -34,20 +33,24 @@ const Search = () => {
         return () => {
             clearTimeout(timeOutId);
         };
-
     }, [term]);
 
-    const renderedLists = results.map((result) => {
+    const renderedLists = results.map(result => {
         return (
             <div className="item" key={result.pageid}>
                 <div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
                 </div>
                 <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                    <div className="header">{result.title}</div>
+                    <span
+                        dangerouslySetInnerHTML={{ __html: result.snippet }}
+                    ></span>
                 </div>
             </div>
         );
@@ -58,18 +61,16 @@ const Search = () => {
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
-                    <input 
-                      className="input"
-                      value={term}
-                      onChange={e => setTerm(e.target.value)} 
+                    <input
+                        className="input"
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
                     />
                 </div>
             </div>
-            <div className="ui celled list">
-                {renderedLists}
-            </div>
+            <div className="ui celled list">{renderedLists}</div>
         </div>
     );
-}
+};
 
 export default Search;
