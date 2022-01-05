653f8e73af08d556375847af9c63fb3947c7b58d
Mehul Shekhar Mukherjee <mehulshekharmukherjee@Mehuls-MacBook-Pro.local>
2022-01-05 16:11:40 -0500

    custom hook created

--- my-youtube/src/components/App.js
+++ my-youtube/src/components/App.js
@@ -1,50 +1,35 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
-class App extends React.Component {
+const App = () => {
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos("Manchester United");
 
-    state = { videos: [], selectedVideo: null };
+    useEffect(() => {
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
-    componentDidMount() {
-        this.onSearch('Manchester United');
-    }
-
-    onSearch = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        this.setState({ 
-            videos: response.data.items,
-            selectedVideo: response.data.items[0] }) ;
-    }
-
-    onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video });
-    }
-
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onSearch={this.onSearch} />
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
-                        </div>                
+    return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={search} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                            onVideoSelect={setSelectedVideo}
+                            videos={videos}
+                        />
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
--- my-youtube/src/components/SearchBar.js
+++ my-youtube/src/components/SearchBar.js
@@ -1,32 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
 
-    state = { term: ''};
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-    onInputChange = (event) => {
-        this.setState({ term: event.target.value });
-    }
+    onFormSubmit(term);
+  };
 
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        this.props.onSearch(this.state.term);
-    }
-    
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input type="text"
-                         value={this.state.term}
-                         onChange={this.onInputChange}/>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
